f0cfc05dcd60e5e42c18ec5c8ba3610154aaa326
neilwebb <neilkwebb@hotmail.com>
2015-07-28 16:09:29 +0000

    Story: artf1930, Author: Neil Webb, Comments: Updated table creation scripts and hibernate configuration files to increase field size on advanced audit tables.

--- Product/DBScripts/nhincdb/nhincdb.sql
+++ Product/DBScripts/nhincdb/nhincdb.sql
@@ -39,14 +39,14 @@ CREATE TABLE IF NOT EXISTS auditrepo.auditrepository
 CREATE TABLE auditrepo.advanced_audit
 (
     audit_id BIGINT NOT NULL,
-    service_name VARCHAR(64),
-    subsystem VARCHAR(16),
-    message_direction VARCHAR(8),
-    message_id VARCHAR(96),
-    user_name VARCHAR(64),
-    user_roles VARCHAR(64),
-    source_system VARCHAR(64),
-    source_community VARCHAR(64),
+    service_name VARCHAR(256),
+    subsystem VARCHAR(256),
+    message_direction VARCHAR(256),
+    message_id VARCHAR(256),
+    user_name VARCHAR(256),
+    user_roles VARCHAR(256),
+    source_system VARCHAR(256),
+    source_community VARCHAR(256),
     query_params TEXT,
     message_audited MEDIUMTEXT,
     PRIMARY KEY (audit_id)
